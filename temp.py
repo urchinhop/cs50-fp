1d1d9fc18f2ae238441ea5b7f7b9b085261c3854
cypher <m18640481597@163.com>
2017-12-23 10:38:32 +0800

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import numpy as np
+import pandas as pd
+
+pd.Series(np.random.randn(5),index=['a','b','c','d','e'])
+s=pd.Series(np.random.randn(5))
+print(s)
